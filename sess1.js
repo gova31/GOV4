dfa57b97980f4bbd46c19c6516b7f771a7cab47f
GOVARDHANA <gova2431@gmail.com>
2025-06-28 19:16:15 +0530

    DAY4 OF TRAINING.

---
+++ sess1.js
@@ -0,0 +1,14 @@
+function alpha() {
+    alert("good nighhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh");
+    var a =prompt("enter a value");
+
+    var b = prompt("enter b value");  
+    var c=parseInt(a) + parseInt(b);
+    alert("the sum is " + c);       
+
+
+}
+// var a = 10;
+// var b = 20; 
+// var c = a + b;
+// console.log(c);
---
+++ sess2.js
@@ -0,0 +1,1 @@
+var beta=(a,b)=> console.log("the sum is " + (parseInt(a) + parseInt(b)));
